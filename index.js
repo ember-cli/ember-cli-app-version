5f7b9954e41288f10df4e8d4a0ee279f163570b0
Taras Mankovski <taras@quandl.com>
2014-11-11 15:52:22 -0500

    Made it possible to call getAppVersion

--- index.js
+++ index.js
@@ -8,23 +8,28 @@ module.exports = {
   config: function(env, baseConfig) {
     this._super.config.apply(this, arguments);
 
-    var version;
-    try {
-      version = execSync('git describe')
-    } catch (e) {
-      // do nothing
-    }
-
-    if (typeof version === 'undefined') {
-      try {
-        version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
-      } catch (e) {
-        // do nothing
-      }
-    }
-
+    var version = getAppVersion();
     if (version) {
       baseConfig.APP.version = version;
     }
-  }
+  },
+  getAppVersion: getAppVersion
 };
+
+function getAppVersion() {
+  var version;
+  try {
+    version = execSync('git describe')
+  } catch (e) {
+    // do nothing
+  }
+
+  if (typeof version === 'undefined') {
+    try {
+      version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
+    } catch (e) {
+      // do nothing
+    }
+  }
+  return version;
+}
