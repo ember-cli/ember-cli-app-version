df03e27de9f58ace8a49486bd2eb690db085845f
Gabor Babicz <gabor.babicz@gmail.com>
2014-11-11 22:50:54 +0100

    Use the app's real name instead of 'App'

--- app/initializers/app-version.js
+++ app/initializers/app-version.js
@@ -1,9 +1,11 @@
 import config from '../config/environment';
 import Ember from 'ember';
+var capitalize = Ember.String.capitalize;
 
 export default {
   name: 'App Version',
-  initialize: function() {
-    Ember.libraries.register('App', config.APP.version);
+  initialize: function(container, application) {
+    var appName = capitalize(application.toString());
+    Ember.libraries.register(appName, config.APP.version);
   }
 }
