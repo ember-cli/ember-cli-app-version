2911dd5a27ea8321eecd46fc5a1bb1f3069b70f0
Taras Mankovski <taras@quandl.com>
2014-11-18 16:39:37 -0500

    Replaced git describe with git-repo-info

--- app/initializers/app-version.js
+++ app/initializers/app-version.js
@@ -1,11 +1,12 @@
 import config from '../config/environment';
 import Ember from 'ember';
-var capitalize = Ember.String.capitalize;
+
+var classify = Ember.String.classify;
 
 export default {
   name: 'App Version',
   initialize: function(container, application) {
-    var appName = capitalize(application.toString());
+    var appName = classify(application.toString());
     Ember.libraries.register(appName, config.APP.version);
   }
 }
--- index.js
+++ index.js
@@ -1,35 +1,18 @@
+/* global module, require */
 'use strict';
-
-var execSync = require('exec-sync');
 var fs = require('fs');
 
 module.exports = {
   name: 'ember-cli-app-version',
   config: function(env, baseConfig) {
-    this._super.config.apply(this, arguments);
+    var config = this._super.config.apply(this, arguments);
 
-    var version = getAppVersion();
+    var version = require('git-repo-version')(null, this.project.root);
     if (version) {
+      baseConfig.APP.name = this.project.pkg.name;
       baseConfig.APP.version = version;
     }
-  },
-  getAppVersion: getAppVersion
+
+    return config;
+  }
 };
-
-function getAppVersion() {
-  var version;
-  try {
-    version = execSync('git describe')
-  } catch (e) {
-    // do nothing
-  }
-
-  if (typeof version === 'undefined') {
-    try {
-      version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
-    } catch (e) {
-      // do nothing
-    }
-  }
-  return version;
-}
