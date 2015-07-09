2fb8104ba35eaa2dab620d6ef7bee519b6eefc59
Taras Mankovski <tarasm@gmail.com>
2015-07-09 16:18:28 -0400

    Fixed index.js

--- index.js
+++ index.js
@@ -2,5 +2,16 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-app-version'
+  name: 'ember-cli-app-version',
+  config: function(env, baseConfig) {
+    var config = this._super.config.apply(this, arguments);
+
+    var version = require('git-repo-version')(null, this.project.root);
+    if (version && baseConfig.APP) {
+      baseConfig.APP.name = this.project.pkg.name;
+      baseConfig.APP.version = version;
+    }
+
+    return config;
+  }
 };
