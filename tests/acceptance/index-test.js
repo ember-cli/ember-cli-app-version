8758cfc8b8710e250ed24ab790558bd14151bf3a
Taras Mankovski <tarasm@gmail.com>
2016-08-09 09:23:37 -0400

    Moved app-version to a helper

---
+++ tests/acceptance/index-test.js
@@ -0,0 +1,16 @@
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import config from 'dummy/config/environment';
+
+moduleForAcceptance('Acceptance | index');
+
+test('visiting /', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+
+    assert.ok(config.APP.version, 'app version is present');
+    assert.equal($('p.message').text().trim(), `Your app version is ${config.APP.version}`);
+  });
+});
