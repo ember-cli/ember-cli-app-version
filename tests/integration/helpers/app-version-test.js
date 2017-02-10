9f66c24faa364cd67a1bdfa473fb8ccbfafa4353
Danielle Adams <danielle.adams@blueapron.com>
2017-02-10 15:15:31 -0500

    import regexp for tests; add expects

--- tests/integration/helpers/app-version-test.js
+++ tests/integration/helpers/app-version-test.js
@@ -1,20 +1,31 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import regexp from 'ember-cli-app-version/utils/regexp';
+
+const {
+  shaRegExp,
+  versionRegExp
+} = regexp;
+
 
 moduleForComponent('Integration | Helper | {{app-version}}', {
   integration: true
 });
 
 test('it displays only app version', function(assert) {
+  assert.expect(2);
+
   this.render(hbs`{{app-version hideSha=true}}`);
 
-  assert.ok(this.$().text().match(/\d[.]\d[.]\d/), 'Displays version.');
-  assert.ok(!this.$().text().match(/[a-z\d]{8}/), 'Does not display git sha.');
+  assert.ok(this.$().text().match(versionRegExp), 'Displays version.');
+  assert.ok(!this.$().text().match(shaRegExp), 'Does not display git sha.');
 });
 
 test('it displays only git sha', function(assert) {
+  assert.expect(2);
+
   this.render(hbs`{{app-version hideVersion=true}}`);
 
-  assert.ok(this.$().text().match(/[a-z\d]{8}/), 'Displays git sha.');
-  assert.ok(!this.$().text().match(/\d[.]\d[.]\d/), 'Does not display version.');
+  assert.ok(this.$().text().match(shaRegExp), 'Displays git sha.');
+  assert.ok(!this.$().text().match(versionRegExp), 'Does not display version.');
 });
--- tests/unit/helpers/app-version-test.js
+++ tests/unit/helpers/app-version-test.js
@@ -11,17 +11,24 @@ const {
 module('Unit | Helper | app version');
 
 test('it returns app version', function(assert) {
+  assert.expect(1);
+
   let result = appVersion();
+
   assert.equal(result, version, 'Returns app version.');
 });
 
 test('it returns only app version', function(assert) {
+  assert.expect(1);
+
   let result = appVersion([], { hideSha: true });
 
   assert.equal(result, version.split('+')[0], 'Returns app version without git sha.');
 });
 
 test('it returns only git sha', function(assert) {
+  assert.expect(1);
+
   let result = appVersion([], { hideVersion: true });
 
   assert.equal(result, version.split('+')[1], 'Returns git sha without app version.');
