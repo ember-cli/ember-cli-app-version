f3c2e1a8e8c52b0e9469fa8c089f57d68958332a
Robert Jackson <me@rwjblue.com>
2017-11-12 10:21:23 -0500

    Avoid errors when `config.APP` is not present.
    The code in `index.js` and `addon/index.js` specifically allows
    `config/environment.js` to not include `APP`, however the use
     of destructuring in the initializer meant that we throw an error
    when initializers are loaded.

--- app/initializers/app-version.js
+++ app/initializers/app-version.js
@@ -1,12 +1,11 @@
 import initializerFactory from 'ember-cli-app-version/initializer-factory';
 import config from '../config/environment';
 
-const {
-  APP: {
-    name,
-    version
-  }
-} = config;
+let name, version;
+if (config.APP) {
+  name = config.APP.name;
+  version = config.APP.version;
+}
 
 export default {
   name: 'App Version',
