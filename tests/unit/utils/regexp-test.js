e3bc9d9f50c7a547696765611a1f110d3db71524
Logan Rosen <logan@ubuntu.com>
2021-11-07 16:28:03 -0500

    Update to ember-cli 3.28 blueprint

--- tests/unit/utils/regexp-test.js
+++ tests/unit/utils/regexp-test.js
@@ -11,8 +11,8 @@ module('Unit | Utility | regexp', function () {
 
     assert.ok('2.0.1'.match(versionRegExp), 'Matches expected pattern.');
     assert.ok('2.20.1'.match(versionRegExp), 'Matches expected pattern.');
-    assert.ok(!'a.b.c'.match(versionRegExp), 'Does not match letters.');
-    assert.ok(!'git12sha'.match(versionRegExp), 'Does not match sha.');
+    assert.notOk('a.b.c'.match(versionRegExp), 'Does not match letters.');
+    assert.notOk('git12sha'.match(versionRegExp), 'Does not match sha.');
   });
 
   test('version extended reg ex matches expected strings', function (assert) {
@@ -26,24 +26,27 @@ module('Unit | Utility | regexp', function () {
       '2.20.1-alpha.15'.match(versionExtendedRegExp),
       'Matches expected pattern.'
     );
-    assert.ok(
-      !'a.b.c-alpha.15'.match(versionExtendedRegExp),
+    assert.notOk(
+      'a.b.c-alpha.15'.match(versionExtendedRegExp),
       'Does not match letters.'
     );
-    assert.ok(!'git12sha'.match(versionExtendedRegExp), 'Does not match sha.');
+    assert.notOk(
+      'git12sha'.match(versionExtendedRegExp),
+      'Does not match sha.'
+    );
   });
 
   test('git sha reg ex matches expected strings', function (assert) {
     assert.expect(4);
 
     assert.ok('git12sha'.match(shaRegExp), 'Matches expected pattern.');
-    assert.ok(!'2.0.1'.match(shaRegExp), 'Does not match version pattern.');
-    assert.ok(
-      !'2.0.1-alpha.15'.match(shaRegExp),
+    assert.notOk('2.0.1'.match(shaRegExp), 'Does not match version pattern.');
+    assert.notOk(
+      '2.0.1-alpha.15'.match(shaRegExp),
       'Does not match version extended pattern.'
     );
-    assert.ok(
-      !'2.0.1-alphaabc.15'.match(shaRegExp),
+    assert.notOk(
+      '2.0.1-alphaabc.15'.match(shaRegExp),
       'Does not match version extended pattern (with 8 chars in tag name).'
     );
   });
